bd07a35058d8fa9fac7c5ab9093814dd4785aeb5
vegaklin <ruslan.evn@mail.ru>
2025-04-20 23:41:50 +0300

    feat: add applications

---
+++ fractal/src/main/java/ru/kfu/fractal/FractalApplication.java
@@ -0,0 +1,11 @@
+package ru.kfu.fractal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FractalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FractalApplication.class, args);
+    }
+}
